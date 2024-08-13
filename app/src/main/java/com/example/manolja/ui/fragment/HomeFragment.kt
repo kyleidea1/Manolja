6328839be4fda612a6bb7af246ff6cd2abfba8ca
김정희 <jenny8530@naver.com>
2024-08-14 03:24:44 +0900

    feat: 메일 버튼 클릭시 퀘스트명 뜨는 기능 추가

--- app/src/main/java/com/example/manolja/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/manolja/ui/fragment/HomeFragment.kt
@@ -4,15 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.manolja.R
 
 class HomeFragment : Fragment() {
 
+    private lateinit var ivMail: ImageView
+    private lateinit var tvQuestPrompt: TextView
+    private lateinit var textContainer: LinearLayout
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        ivMail = view.findViewById(R.id.ivMail)
+        tvQuestPrompt = view.findViewById(R.id.tvQuestPrompt)
+        textContainer = view.findViewById(R.id.textContainer)
+
+        ivMail.setOnClickListener {
+            tvQuestPrompt.visibility = View.INVISIBLE
+            ivMail.visibility = View.INVISIBLE
+            textContainer.visibility = View.VISIBLE
+        }
+
+        return view
     }
 }
